8557e70784c54eec9000e61c6cd2997af03344b5
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-09-20 12:44:38 +0300

    Nuget update

--- src/Lykke.Job.SqlBridgeChecker/Program.cs
+++ src/Lykke.Job.SqlBridgeChecker/Program.cs
@@ -8,7 +8,7 @@ namespace Lykke.Job.SqlBridgeChecker
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine($"{AppEnvironment.Name} version {AppEnvironment.Version}");
 #if DEBUG
